24b0e4d2d9844bd498157b003a697492f51dcc65
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-25 21:52:45 +0100

    [UPD] LAMPADINA ACCESA E SPENTA DOPO AVER CLICCATO SUL BOTTONE

--- script.js/js.js
+++ script.js/js.js
@@ -1,10 +1,12 @@
-const immagine = document.querySelector('img');
-const button = document.getElementById('change_color');
-button.addEventListener('click', function(){
-    if(immagine.classList.contains()){
-        immagine.classList.remove();
+const immagine = document.getElementById('image');
+const button = document.getElementById('bottone');
+button.addEventListener('click', function () {
+    if (immagine.classList.contains(immagine.src='./img/yellow_lamp.png')) {
+        immagine.classList.remove(immagine.src='./img/yellow_lamp.png');
+        immagine.classList.add(immagine.src='./img/white_lamp.png');
     }
     else {
-    immagine.classList.add();
+        immagine.classList.remove(immagine.src='./img/white_lamp.png');
+        immagine.classList.add(immagine.src='./img/yellow_lamp.png');
     }
- });
+});
