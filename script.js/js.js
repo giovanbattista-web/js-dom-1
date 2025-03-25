0e26f0adf26e9fd3cafd7058ae5307831e796d9b
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-25 18:00:04 +0100

    [UPD] ULTERIORE AGGIORNAMENTO

--- script.js/js.js
+++ script.js/js.js
@@ -1,9 +1,6 @@
-const greetings = document.getElementById('saluta');
-console.log(greetings);
-
 const immagine = document.querySelector('img');
 const button = document.getElementById('change_color');
-button.AddEventListener('click', function(){
+button.addEventListener('click', function(){
     if(immagine.classList.contains()){
         immagine.classList.remove();
     }
