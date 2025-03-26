fb8f969c6ccd4c2cb78dd1937568295bda49757d
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-26 06:50:12 +0100

    [UPD] ULTERIORE MODIFICA

--- script.js/js.js
+++ script.js/js.js
@@ -1,17 +1,17 @@
 const immagine = document.getElementById('image');
 const button = document.getElementById('bottone');
-button.addEventListener('click', function (){
+button.addEventListener('click', function () {
+    /*
+    if (immagine.classList.contains(immagine.src='./img/yellow_lamp.png')) {
+            immagine.classList.remove(immagine.src='./img/yellow_lamp.png');
+            immagine.classList.add(immagine.src='./img/white_lamp.png');
+        }
+        else {
+            immagine.classList.remove(immagine.src='./img/white_lamp.png');
+            immagine.classList.add(immagine.src='./img/yellow_lamp.png');
+        }
+    */
+    immagine.classList.toggle(immagine.src = './img/yellow_lamp.png');
 
-if (immagine.classList.contains(immagine.src='./img/yellow_lamp.png')) {
-        immagine.classList.remove(immagine.src='./img/yellow_lamp.png');
-        immagine.classList.add(immagine.src='./img/white_lamp.png');
-    }
-    else {
-        immagine.classList.remove(immagine.src='./img/white_lamp.png');
-        immagine.classList.add(immagine.src='./img/yellow_lamp.png');
-    }
-/*
-immagine.classList.toggle(immagine.src='./img/yellow_lamp.png');
-*/
 });
 
