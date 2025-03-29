a86d389b3069b4759507041c4544deeeb622602e
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-29 16:24:36 +0100

    [UPD] CORREZIONE DEL PROFESSORE

--- script.js/js.js
+++ script.js/js.js
@@ -1,17 +1,32 @@
-const immagine = document.getElementById('image');
-const button = document.getElementById('bottone');
+// RECUPERO L'IMMAGINE ED IL PULSANTE DAL DOM
+const image = document.getElementById('off-lamp');
+// const offLamp = document.getElementById('off-lamp');
+// const onLamp = document.getElementById('on-lamp');
+const button = document.getElementById('on-off');
+
+// console.log(offLamp);
+// console.log(onLamp);
+// console.log(button);
+
+// DEVO GESTIRE IL CLICK SUL PULSANTE 
 button.addEventListener('click', function () {
-    /*
-    if (immagine.classList.contains(immagine.src='./img/yellow_lamp.png')) {
-            immagine.classList.remove(immagine.src='./img/yellow_lamp.png');
-            immagine.classList.add(immagine.src='./img/white_lamp.png');
-        }
-        else {
-            immagine.classList.remove(immagine.src='./img/white_lamp.png');
-            immagine.classList.add(immagine.src='./img/yellow_lamp.png');
-        }
-    */
-    immagine.classList.toggle(immagine.src = './img/yellow_lamp.png');
+  // CLICCATO IL PULSANTE DEVO CAMBIARE IL VALORE DELL'ATTRIBUTO src DELL'IMMAGINE
+  // SE L'IMMAGINE E' QUELLA DELLA LAMPADINA GIALLA , VUOL DIRE CHE E' ACCESA QUINDI DEVO SPEGNERLA,
+  // ALTRIMENTI VICEVERSA
+  if(image.src.includes('yellow')){
+    image.src = './img/white_lamp.png';
+    button.innerText = 'Accendi';
+  }
+  else 
+  {
+    image.src = './img/yellow_lamp.png';
+    button.innerText = 'Spegni';
+  }
 
+
+
+
+
+// onLamp.classList.remove('display-none');
+// offLamp.classList.add('display-none');
 });
-
